e2217932c62b974a9e31448763a0c7d572c14f32
SayaniSen2022 <sayanisen.official@gmail.com>
2023-08-02 01:05:09 +0530

    update AutoResponse

--- client/src/components/AutoResponse.tsx
+++ client/src/components/AutoResponse.tsx
@@ -5,16 +5,23 @@ import SendIcon from '@mui/icons-material/Send';
 
 
 const AutoResponse = () => {
-    interface MessageHistory {
+    interface SenderHistory {
     content: string;
     sender: string;
     timestamp: string;
   }
+  interface ReceiverHistory {
+    content: string;
+    receiver: string;
+    timestamp: string;
+  }
+
   type Message = string;
 
   const[message, setMessage] = useState<Message>('');
   const[response, setResponse] = useState<Message>('');
-  const[chatRecord, setChatRecord] = useState<MessageHistory[]>([]);
+  const[chatRecord, setChatRecord] = useState<SenderHistory[]>([]);
+  const[responseRecord, setResponseRecord] = useState<ReceiverHistory[]>([]);
 
   const demoResponseSet: string[] = ["Hello", "How can I assist you?", "Have a good day!"];
 
@@ -28,18 +35,30 @@ const handleResponseReceived = () => {
     let responseIndex: number = Math.floor(Math.random()* demoResponseSet.length);
     let newRes = demoResponseSet[responseIndex];
 
-    const newMessageObject: MessageHistory = {
+    const newMessageObject: SenderHistory = {
       content: message.trim(),
       sender: "You",
       timestamp: new Date().toLocaleString(),
     }
+    const newResponseObject: ReceiverHistory = {
+      content: newRes,
+      receiver: "Zimi",
+      timestamp: new Date().toLocaleString(),
+    }
     setChatRecord([...chatRecord, newMessageObject])
     setResponse(newRes);
+    setResponseRecord([...responseRecord, newResponseObject])
     setMessage('');
   }
 };
 
-console.log(chatRecord)
+// console.log(chatRecord, responseRecord);
+
+const chatRecordString: string = JSON.stringify(chatRecord);
+localStorage.setItem("SenderHistory", chatRecordString);
+const responseRecordString: string = JSON.stringify(responseRecord);
+localStorage.setItem("Receiver History", responseRecordString);
+
 
 const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
   if (event.key === 'Enter') {
